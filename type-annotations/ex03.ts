4f40f20e6c78efba42a2fc8efb29376a219ba5fd
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 08:48:05 -0300

    Adding exercise 3 on type annotations, where I had to create a sum function using type annotations

---
+++ type-annotations/ex03.ts
@@ -0,0 +1,27 @@
+/*
+Exercise 3 - Function
+
+Create a function called sum.
+
+It receives:
+
+two numbers
+
+And returns:
+
+their sum.
+
+Use type annotations on the parameters and the return value.
+*/
+
+function sum(
+    
+    number1: number,
+    number2: number
+
+): number {
+
+    return number1 + number2;
+};
+
+console.log(sum(2, 2))
