c30a18d0c0036217886a81a1bd2943d46ab488bc
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 08:51:01 -0300

    Adding exercise 3 on type annotations where I had to create a function called greet and return a greet using a name, all this usin type annotations

---
+++ type-annotations/ex-04.ts
@@ -0,0 +1,21 @@
+/*
+Exercise 4 - Greeting
+
+Create a function called greet.
+
+It receives:
+
+a name (string)
+
+And returns:
+
+Hello Pedro!
+
+Use type annotations.
+*/
+
+function greet(name: string): string {
+    return `Hello ${name}!`
+}
+
+console.log(greet('Neymar'))
