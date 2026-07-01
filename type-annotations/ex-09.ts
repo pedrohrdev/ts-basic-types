441947889bbb762a9227e2966269dbb0a5886b78
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 09:44:51 -0300

    Adding exercise 9 where i had do create an array of objects using type annotations

---
+++ type-annotations/ex-09.ts
@@ -0,0 +1,27 @@
+/*
+Exercise 9 - User List
+
+Create an array of objects.
+
+Each object contains:
+
+{
+name: string
+age: number
+}
+*/
+
+let userList: {
+        nameUser: string;
+        age: number
+}[] = [   
+
+    {
+       nameUser: "Pedro",
+       age: 10 
+    }
+
+];
+
+console.log(userList)
+
