c1d75f79acbf90a8b60ba0dce58135fd10f39507
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 08:36:32 -0300

    Adding the lesson 1

---
+++ type-annotations/ex01.ts
@@ -0,0 +1,13 @@
+/*
+Exercise 1 - Variables
+
+Create the following variables using type annotations:
+
+name (string)
+age (number)
+studying (boolean)
+*/
+
+let userName: string = "Pedro";
+let age: number = 16;
+let studying: boolean = true
