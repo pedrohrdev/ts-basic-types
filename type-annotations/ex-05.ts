034bad4747bdf26906ad01009e051479ee2e41ca
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 09:05:09 -0300

    Adding exercise 05 where i had to create a object called 'usuario' in wich all fields must have types

---
+++ type-annotations/ex-05.ts
@@ -0,0 +1,19 @@
+/*
+Exercise 5 - Object
+
+Create a variable named `usuario` in which all fields must have types.
+*/
+
+const usuario: {
+    username: string;
+    age: number;
+    isProgrammer: boolean;
+} = {
+    username: "Pedro",
+    age: 16,
+    isProgrammer: true
+}
+
+
+
+console.log(usuario)
