852f4674f722c3016203f47d59cd936469be2f6b
pedrohrdev <pedrohrdev@gmail.com>
2026-07-01 08:29:38 -0300

    Adding the concept of type annotations

---
+++ type-annotations-sintaxe.ts
@@ -0,0 +1,11 @@
+// What are type annotations?
+// Type annotations are a feature in TypeScript 
+// that allow developers to explicitly specify
+// the types of variables, function parameters,
+// return values, and other data structures. By
+// providing type information, TypeScript can
+// perform static type checking,
+// which helps catch errors at compile time rather
+// than at runtime. This leads to more robust and
+// maintainable code.
+
