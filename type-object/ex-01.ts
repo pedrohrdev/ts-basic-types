8a6c2a6077b1df233560c5eb6374b5d10e227d63
pedrohrdev <pedrohrdev@gmail.com>
2026-07-02 09:59:40 -0300

    Fix exercise 1

--- type-object/ex-01.ts
+++ type-object/ex-01.ts
@@ -16,10 +16,18 @@ run `console.log(book);`
 Do not try to access the properties; just print the entire object.
 */
 
-const book = {
+const book: object = {
     name: "Can't Hurt Me",
     author: "David Goggins",
     pages: 364
 };
 
 console.log(book);
+// Showing the entire object works.
+
+console.log(book.name); // This will cause a compile-time error, as
+// the `name` property is not accessible on type `object`.
+
+// This is precisely the behavior we wanted to observe: when using
+// `object`, TypeScript only knows that the variable holds an
+// object, but it does not know which properties it has.
