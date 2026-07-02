933002b3dd03446f5199d0ae7337661e32100141
pedrohrdev <pedrohrdev@gmail.com>
2026-07-02 09:35:30 -0300

    Adding exercice 1 on void, where i had to create a function called greeting and dont return anything, just greet

---
+++ type-void/ex-01.ts
@@ -0,0 +1,17 @@
+/*
+Exercise 1 - Greeting
+
+Create a function named sayHello
+
+It should simply print:
+
+Hello, Pedro!
+
+The function must not return anything.
+*/
+
+function greeting(): void {
+    console.log('Hello, Pedro!');
+}
+
+greeting()
