cc5b7f619b158fbd94c800b617952f5def19c1ec
pedrohrdev <pedrohrdev@gmail.com>
2026-07-02 09:56:54 -0300

    Adding exercise 1 on objects, where i need to create a object and show it

---
+++ type-object/ex-01.ts
@@ -0,0 +1,25 @@
+/*
+Exercise 1 
+
+Create a variable named `book` of type `object`.
+
+It should store:
+
+title
+author
+pages
+
+Then:
+
+run `console.log(book);`
+
+Do not try to access the properties; just print the entire object.
+*/
+
+const book = {
+    name: "Can't Hurt Me",
+    author: "David Goggins",
+    pages: 364
+};
+
+console.log(book);
