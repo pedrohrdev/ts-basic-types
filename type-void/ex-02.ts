75172ae1bd64af655b6afe2f09cd3f2ac26abadc
pedrohrdev <pedrohrdev@gmail.com>
2026-07-02 09:38:16 -0300

    Adding exercise 2 on void, where i had to create a function called sum, and just show the sum of 2 numbers, dont return it

---
+++ type-void/ex-02.ts
@@ -0,0 +1,27 @@
+/*
+Exercise 2 - Simple calculator
+
+Create a function called sum, It should calculate
+the sum and display it in the console.
+
+Example:
+
+Result: 15
+
+But do not use `return`.
+
+Then, call the function a few times.
+*/
+
+function sum(
+    number1: number,
+    number2: number
+): void {
+
+    console.log(number1 + number2)
+
+};
+
+sum(1, 1);
+sum(24, 32);
+sum(123, 346);
