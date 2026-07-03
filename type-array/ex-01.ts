d08d406ad0dc1e9d3cfb5d9f7724fc3591deecff
pedrohrdev <pedrohrdev@gmail.com>
2026-07-03 10:28:21 -0300

    Adding Exercise 1 from the array types lesson, where I had to create an array of products, then add one and log it to the console before and after the push.

---
+++ type-array/ex-01.ts
@@ -0,0 +1,60 @@
+/*
+Exercise 1 — Products
+
+Create an array named `products` containing at least 4 products.
+
+Each product must have:
+
+name
+price
+stock
+
+Then:
+
+display the array in the console;
+add one more product using `push()`.
+*/
+
+let products: {
+
+    name: string;
+    price: number;
+    stock: boolean
+
+}[] = [
+    {
+        name: "Monster mango",
+        price: 9.89,
+        stock: true
+    },
+
+    {
+        name: "Snickers",
+        price: 4.99,
+        stock: false
+    },
+
+    {
+        name: "Nike Air Jordan",
+        price: 498.99,
+        stock: true
+    },
+
+    {
+        name: "GTA VI",
+        price: 499.99,
+        stock: true 
+    }
+];
+
+console.log(products);
+
+products.push(
+    {
+        name: "Mate Erva",
+        price: 21.50,
+        stock: true
+    }
+)
+
+console.log(products)
