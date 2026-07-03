99d9f730e8fa9f9e8eee4c2fafc6ddb8009e22ac
pedrohrdev <pedrohrdev@gmail.com>
2026-07-03 10:20:36 -0300

    Adding my lesson about Type Array

---
+++ type-array/index.ts
@@ -0,0 +1,13 @@
+// The type Array is a utility type that represents an array of a specific type. It is defined as follows:
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+let strings: string[] = ["hello", "world"];
+
+// You can also use the Array<T> syntax to define an array of a specific type:
+
+let booleans: Array<boolean> = [true, false, true];
+
+// The type Array can also be used with generics to create arrays of any type, but normally
+// the type[] syntax is preferred for its simplicity and readability.
+
