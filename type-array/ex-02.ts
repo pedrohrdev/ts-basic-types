af06092563f1e20114cc694f89505e11d7a772df
pedrohrdev <pedrohrdev@gmail.com>
2026-07-03 10:42:34 -0300

    Adding Exercise 2 to the lesson on arrays, where I had to create an array of movies, display all of them, and then show only the ones you've already watched.

---
+++ type-array/ex-02.ts
@@ -0,0 +1,55 @@
+/*
+Exercise 2 — Movies
+
+Create an array named `movies` containing objects with:
+
+title
+year
+watched
+
+Then:
+
+iterate through the array using `for...of`;
+print only the title and year of each movie;
+at the end, count how many movies have been watched (`watched === true`) and display that number.
+*/
+
+// Type Movie containing the parameters of movie
+type Movie = {
+    title: string,
+    year: number,
+    watched: boolean
+}
+
+let movies: Movie[] = [
+    {
+        title: "Spider-Man: No Way Home",
+        year: 2021,
+        watched: true
+    },
+
+        {
+        title: "Grown Ups",
+        year: 2010,
+        watched: true
+    },
+
+        {
+        title: "Fight Club",
+        year: 1999,
+        watched: false
+    }
+];
+
+for(const movie of movies) {
+    console.log(`${movie.title}, year: ${movie.year};`);
+};
+
+console.log('============================')
+
+for(const movieWatched of movies) {
+    if(movieWatched.watched === true) {
+        console.log(`you watched: ${movieWatched.title}`)
+    }    
+}
+
