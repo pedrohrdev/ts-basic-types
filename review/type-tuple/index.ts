9d5d56c24012832fef1dfe015d44438dcd09f6c1
pedrohrdev <pedrohrdev@gmail.com>
2026-07-13 10:56:35 -0300

    Review the Type Tuple

---
+++ review/type-tuple/index.ts
@@ -0,0 +1,11 @@
+// Type Tuple
+
+let palette: readonly [string, string, string, string, string];
+
+palette = ["#fff", "#f2f0f3", "#1a161c", "#ffffff", "#6f6d73"];
+
+console.log(palette);
+
+palette.push('#6f6d43'); // This will return a error
+
+console.log(palette)
