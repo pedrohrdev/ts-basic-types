b2d6069d810ec8826fc935d9802d5025662b8042
pedrohrdev <pedrohrdev@gmail.com>
2026-07-13 10:34:46 -0300

    Review the Type Object

---
+++ exercices/level1/ex05.ts
@@ -0,0 +1,29 @@
+/*
+Exercise 5
+
+You received data from an API:
+
+let data: unknown = {
+  id: 1,
+  title: "Learn TypeScript",
+  completed: false
+};
+
+Do the following:
+
+1. Use a type assertion (as) to treat `data` as an object with:
+   - id: number
+   - title: string
+   - completed: boolean
+
+2. Print:
+
+Task: Learn TypeScript
+Completed: No
+
+Rules:
+❌ Do not use `any`.
+✅ Use `as`.
+✅ Do not change the original type of `data`.
+*/
+
---
+++ review/type-object/index.ts
@@ -0,0 +1,13 @@
+// Type Object 
+
+type person = {
+    name: string,
+    age: number,
+    working: boolean
+};
+
+const user6: person = {
+    name: "Pedro",
+    age: 16,
+    working: false
+}
